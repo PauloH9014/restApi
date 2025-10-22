1a4105ccb51417060166b73f85cc388384e22748
PH <paulofrf40@gmail.com>
2025-10-22 16:46:29 -0300

    retirei codigos mortos, para que ficasse com uma estrutura mais limpa.

--- src/main/java/com/paulo/productapi/ProductService/ProductService.java
+++ src/main/java/com/paulo/productapi/ProductService/ProductService.java
@@ -1,8 +1,6 @@
-// File: Service.java (Nomeie como ProductService.java para clareza)
 
 package com.paulo.productapi.ProductService;
 
-// 🚨 CORREÇÃO: Use o caminho e o nome corretos
 import com.paulo.productapi.ProductModel.ProductModel;
 import com.paulo.productapi.ProductRepository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,17 +14,14 @@ public class ProductService {
     @Autowired
     private ProductRepository repositoryProduct;
 
-    // 🚨 CORREÇÃO: O tipo de retorno e de argumento é ProductModel
     public ProductModel save(ProductModel productModel){
         return repositoryProduct.save(productModel);
     }
 
-    // 🚨 CORREÇÃO: O tipo da lista é ProductModel
     public List<ProductModel> findAll(){
         return repositoryProduct.findAll();
     }
 
-    // 🚨 CORREÇÃO: O tipo de retorno é ProductModel
     public ProductModel findById(Long id){
         return repositoryProduct.findById(id).orElse(null);
     }
